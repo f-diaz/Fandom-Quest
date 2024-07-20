2aebada110db043b4c89abc7eb7af2a9089f4e84
Fernando Diaz <hola@fernandodiaz.dev>
2024-07-20 19:41:49 +0200

    Initial Commit

---
+++ FandomQuest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FandomQuest
+//
+//  Created by Fernando Diaz de Tudela on 20/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FandomQuest/FandomQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FandomQuestApp.swift
+//  FandomQuest
+//
+//  Created by Fernando Diaz de Tudela on 20/7/24.
+//
+
+import SwiftUI
+
+@main
+struct FandomQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
