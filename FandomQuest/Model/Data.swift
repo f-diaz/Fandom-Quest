7a2ed581e0fdf583b67232b9feb2644a4be1d7e1
Fernando Diaz <hola@fernandodiaz.dev>
2024-07-20 21:46:38 +0200

    Añadidos más juegos.

---
+++ FandomQuest/Model/Data.swift
@@ -0,0 +1,8 @@
+//
+//  Data.swift
+//  FandomQuest
+//
+//  Created by Fernando Diaz de Tudela on 20/7/24.
+//
+
+import Foundation
---
+++ FandomQuest/Model/Game.swift
@@ -0,0 +1,8 @@
+//
+//  Game.swift
+//  FandomQuest
+//
+//  Created by Fernando Diaz de Tudela on 20/7/24.
+//
+
+import Foundation
